c837960577c6d69874845d303677e1e4d7f2a85c
jonkatz2 <jonkatz2@gmail.com>
2016-01-28 14:38:43 -0500

    packagify

--- R/carouselUI.R
+++ R/carouselUI.R
@@ -1,46 +1,46 @@
-#'carouselUI
-#' 
-#'Make a dynamic carousel for use with shiny::renderUI() and shiny::uiOutput(). 
-#' 
-#'@param slides A list of HTML tag elements, e.g. \code{list(shiny::div(), shiny::div())}.
-#'@param data.interval Milliseconds to pause before auto advancing (numeric).
-#'@param data.ride Either \code{carousel} or \code{NA}; unclear if this works.
-#'@return HTML code with class \code{shiny.tag} that can be passed to \code{uiOutput}. 
+#' carouselUI
+#'  
+#' Make a dynamic carousel for use with shiny::renderUI() and shiny::uiOutput(). 
+#'  
+#' @param slides A list of HTML tag elements, e.g. \code{list(shiny::div(), shiny::div())}.
+#' @param data.interval Milliseconds to pause before auto advancing (numeric).
+#' @param data.ride Either \code{carousel} or \code{NA}; unclear if this works.
+#' @return HTML code with class \code{shiny.tag} that can be passed to \code{uiOutput}. 
 #' @author Jon Katz
-#'@note Adapted by JEK from \url{https://github.com/dcurrier/carouselPanel/blob/master/carouselPanel.R}
-#'@examples
-#'c.slides <- lapply(1:5, function(x) div(h3(style='text-align:center;',paste('Slide', x))))
-#'carouselUI(slides=c.slides, data.interval=5000, data.ride=FALSE)
-#'
-#'\dontrun{
-#'/*     place in www/css/styles.css:    */
-#'    .carousel-indicators li {
-#'        background-color: #DDD;
-#'        background-color: rgba(70,70,70,.25);
-#'    }
-#'    .carousel-indicators .active {
-#'        background-color: #999;
-#'    }
-#'    .carousel-indicators {
-#'        bottom: 15px !important;
-#'        top: auto;
-#'        list-style: none outside none;
-#'        margin: 0;
-#'        position: absolute;
-#'        right: 70px;
-#'        z-index: 5;
-#'    }
-#'    .carousel-control {
-#'        opacity: 0.2;
-#'        border: none;
-#'        top: auto;
-#'    }
-#'    .item {
-#'        padding: 0px 17px;
-#'    }
-#'}
-#'@keywords manip
-#'@export
+#' @note Adapted by JEK from \url{https://github.com/dcurrier/carouselPanel/blob/master/carouselPanel.R}
+#' @examples
+#' c.slides <- lapply(1:5, function(x) div(h3(style='text-align:center;',paste('Slide', x))))
+#' carouselUI(slides=c.slides, data.interval=5000, data.ride=FALSE)
+#' 
+#' \dontrun{
+#' /*     place in www/css/styles.css:    */
+#'     .carousel-indicators li {
+#'         background-color: #DDD;
+#'         background-color: rgba(70,70,70,.25);
+#'     }
+#'     .carousel-indicators .active {
+#'         background-color: #999;
+#'     }
+#'     .carousel-indicators {
+#'         bottom: 15px !important;
+#'         top: auto;
+#'         list-style: none outside none;
+#'         margin: 0;
+#'         position: absolute;
+#'         right: 70px;
+#'         z-index: 5;
+#'     }
+#'     .carousel-control {
+#'         opacity: 0.2;
+#'         border: none;
+#'         top: auto;
+#'     }
+#'     .item {
+#'         padding: 0px 17px;
+#'     }
+#' }
+#' @keywords manip
+#' @export
 
 carouselUI <- function(slides, data.interval=10000, data.ride="carousel"){
     if(!is.list(slides)) stop("'slides' must be a list in which each element becomes a slide.")
