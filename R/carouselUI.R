97f4c40fac61fa751d6b396b142ad14a7e03b72a
jonkatz2 <jonkatz2@gmail.com>
2016-01-28 14:24:19 -0500

    packagify

--- R/carouselUI.R
+++ R/carouselUI.R
@@ -1,75 +1,85 @@
-# Make a dynamic carousel for use with renderUI() and uiOutput() 
-# Adapted by JEK from https://github.com/dcurrier/carouselPanel/blob/master/carouselPanel.R
-
-# slides is a list of HTML tag elements, e.g. list(div(), div())
-# data.interval is a number, in milliseconds
-# data.ride is either "carousel" or NA; unclear if this works 
-
-#/*     in www/css/styles.css:    */
-#    .carousel-indicators li {
-#        background-color: #DDD;
-#        background-color: rgba(70,70,70,.25);
-#    }
-#    .carousel-indicators .active {
-#        background-color: #999;
-#    }
-#    .carousel-indicators {
-#        bottom: 15px !important;
-#        top: auto;
-#        list-style: none outside none;
-#        margin: 0;
-#        position: absolute;
-#        right: 70px;
-#        z-index: 5;
-#    }
-#    .carousel-control {
-#        opacity: 0.2;
-#        border: none;
-#        top: auto;
-#    }
-#    .item {
-#        padding: 0px 17px;
-#    }
+#'carouselUI
+#' 
+#'Make a dynamic carousel for use with shiny::renderUI() and shiny::uiOutput(). 
+#' 
+#'@param slides A list of HTML tag elements, e.g. \code{list(shiny::div(), shiny::div())}.
+#'@param data.interval Milliseconds to pause before auto advancing (numeric).
+#'@param data.ride Either \code{carousel} or \code{NA}; unclear if this works.
+#'@return HTML code with class \code{shiny.tag} that can be passed to \code{uiOutput}. 
+#'@note 'Adapted by JEK from https://github.com/dcurrier/carouselPanel/blob/master/carouselPanel.R
+#'@examples
+#'c.slides <- lapply(1:5, function(x) div(h3(style='text-align:center;',paste('Slide', x))))
+#'carouselUI(slides=c.slides, data.interval=5000, data.ride=FALSE)
+#'
+#'\dontrun{
+#'/*     in www/css/styles.css:    */
+#'    .carousel-indicators li {
+#'        background-color: #DDD;
+#'        background-color: rgba(70,70,70,.25);
+#'    }
+#'    .carousel-indicators .active {
+#'        background-color: #999;
+#'    }
+#'    .carousel-indicators {
+#'        bottom: 15px !important;
+#'        top: auto;
+#'        list-style: none outside none;
+#'        margin: 0;
+#'        position: absolute;
+#'        right: 70px;
+#'        z-index: 5;
+#'    }
+#'    .carousel-control {
+#'        opacity: 0.2;
+#'        border: none;
+#'        top: auto;
+#'    }
+#'    .item {
+#'        padding: 0px 17px;
+#'    }
+#'}
+#'@keywords manip
+#'@export
 
 carouselUI <- function(slides, data.interval=10000, data.ride="carousel"){
     if(!is.list(slides)) stop("'slides' must be a list in which each element becomes a slide.")
     # Each slide corresponds to one level of a list
-    active <- list(div(class="item active", slides[[1]]))
+    active <- list(shiny::div(class="item active", slides[[1]]))
     items <- lapply(slides[2:length(slides)], function(x) {
-        div(class="item", x)
+        shiny::div(class="item", x)
     })
     # A random string that always starts with a letter
     carouselID <- rName()
     #Set up carousel
-    div(id=paste0("carousel-", carouselID), class="carousel slide", "data-interval"=as.character(data.interval), "data-ride"=as.character(data.ride), 
+    shiny::div(id=paste0("carousel-", carouselID), class="carousel slide", "data-interval"=as.character(data.interval), "data-ride"=tolower(as.character(data.ride)), 
         # Carousel Inner Div - contains the content to display            
-        div(class="carousel-inner", style="padding-bottom:40px;", active, items),
+        shiny::div(class="carousel-inner", style="padding-bottom:40px;", active, items),
 
         # Carousel controls
-        a(class="left carousel-control",
+        shiny::a(class="left carousel-control",
             "data-slide"="prev",
             href=paste0("#carousel-", carouselID),
             style="background: transparent; color: #000",
-            HTML(paste0("<i class='fa fa-chevron-left'></i>")) 
+            shiny::HTML(paste0("<i class='fa fa-chevron-left'></i>")) 
         ),
 
-        a(class="right carousel-control",
+        shiny::a(class="right carousel-control",
             "data-slide"="next",
             href=paste0("#carousel-", carouselID),
             style="background: transparent; color: #000",
-            HTML(paste0("<i class='fa fa-chevron-right'></i>")) 
+            shiny::HTML(paste0("<i class='fa fa-chevron-right'></i>")) 
         ),
 
         # Generate the carousel indicators
-        HTML("<ol class='carousel-indicators'>"),
-        tag('li', list(class='active', "data-slide-to"=paste(0),
+        shiny::HTML("<ol class='carousel-indicators'>"),
+        shiny::tag('li', list(class='active', "data-slide-to"=paste(0),
             "data-target"=paste0("#carousel-", carouselID))
         ),
         mapply(function(i){
-           list(tag('li', list(class='', "data-slide-to"=paste(i),
+           list(shiny::tag('li', list(class='', "data-slide-to"=paste(i),
               "data-target"=paste0("#carousel-", carouselID))) )
         }, 1:(length(slides)-1), SIMPLIFY=F, USE.NAMES=F),
-        HTML("</ol>")
+        shiny::HTML("</ol>")
     )
 }
 
--- R/popoverInput.R
+++ R/popoverInput.R
@@ -1,33 +1,40 @@
-# A Function to place a help icon with popover text next to a shiny input function
-# Requires Eric Bailey's shinyBS package (available from CRAN) 
-
-# fnType: function or function name (character) of desired input, e.g. textInput or 'textInput'
-# id: inputID (character)
-# label: input label (character)
-# \dots: additional named arguments to input function
-# helpText: character, text to place in popover
-# divStyle: character or NULL, inline CSS for main div returned
-# helpStyle: character or NULL, inline CSS for the help icon (e.g. padding to align it vertically with input)
-# popTrigger: character, trigger for popover. in c('hover','focus','click','manual') 
-# colWidths: length 2 numeric vector specifying width of help button column and width of input function column. Should sum to 12.
-
-# EXAMPLE
-#    popoverInput(
-#        checkboxGroupInput, 
-#        'lyrOpts', 
-#        'Options:',
-#        choices=c(
-#            'Paste URL'='url',
-#            'Clip to Layer'='clip', 
-#            'Projection Ref'='projection', 
-#            'No Data'='empty'
-#            ), 
-#        selected=c('clip','projection'), 
-#        inline=TRUE,
-#        helpText="To download a layer from a website check \"Paste URL\". You may also clip or project all rasters to match this one. If the layer contains only boundary or projection information check \"No Data\".",
-#        divStyle='padding-top:1em;padding-bottom:1em;',
-#        helpStyle='padding-top:0.5em;'
-#    )
+#' popoverInput
+#' 
+#' Place a help icon with popover text next to a shiny input function
+#' 
+#' 
+#' @param fnType function or function name (character) of desired input, e.g. textInput or 'textInput'
+#' @param id InputID for shiny element (character).
+#' @param label Input label (character).
+#' @param \dots Additional named arguments to input function.
+#' @param helpText Text to place in popover (character).
+#' @param divStyle Inline CSS for main div returned (character or NULL).
+#' @param helpStyle Inline CSS for the help icon (e.g. padding to align it vertically with input; character or NULL).
+#' @param popTrigger Trigger for popover. In c('hover','focus','click','manual').
+#' @param colWidths Length 2 numeric vector specifying width of help button column and width of input function column. Should sum to 12.
+#' @return HTML code. 
+#' @note Requires Eric Bailey's shinyBS package (available from CRAN).
+#' @examples
+#'    popoverInput(
+#'        checkboxGroupInput, 
+#'        'lyrOpts', 
+#'        'Options:',
+#'        choices=c(
+#'            'Paste URL'='url',
+#'            'Clip to Layer'='clip', 
+#'            'Projection Ref'='projection', 
+#'            'No Data'='empty'
+#'            ), 
+#'        selected=c('clip','projection'), 
+#'        inline=TRUE,
+#'        helpText="To download a layer from a website check \"Paste URL\". You may also clip or project all rasters to match this one. If the layer contains only boundary or projection information check \"No Data\".",
+#'        divStyle='padding-top:1em;padding-bottom:1em;',
+#'        helpStyle='padding-top:0.5em;'
+#'    )
+#' @keywords manip
+#' @export
+#' 
+#' 
 
 popoverInput <- function(
     fnType,
@@ -41,14 +48,14 @@ popoverInput <- function(
     colWidths=c(2,10)
 ) {
     fnParams <- list(...)
-    div(style=divStyle,
-        column(width=colWidths[1],
-            div(style=helpStyle, 
-                actionLink(paste0(id,'Help'),'',icon=icon('question-circle'))
+    shiny::div(style=divStyle,
+        shiny::column(width=colWidths[1],
+            shiny::div(style=helpStyle, 
+                shiny::actionLink(paste0(id,'Help'),'',icon=icon('question-circle'))
             ),
-            bsPopover(id=paste0(id,'Help'), title=NULL, content=helpText, trigger=popTrigger, options=list(container='body'))
+            shinyBS::bsPopover(id=paste0(id,'Help'), title=NULL, content=helpText, trigger=popTrigger, options=list(container='body'))
         ),
-        column(width=colWidths[2], 
+        shiny::column(width=colWidths[2], 
             do.call(fnType,c(list(inputId=id,label=label), fnParams))
         ) 
     )
