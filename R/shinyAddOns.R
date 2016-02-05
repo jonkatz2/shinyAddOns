a38d592942501afed085035833a98d8422ceed94
jonkatz2 <jonkatz2@gmail.com>
2016-02-05 11:49:35 -0500

    update

---
+++ R/shinyAddOns.R
@@ -0,0 +1,9 @@
+#' A Mish-Mash Of Functions Useful In Shiny Apps
+#'
+#' These functions aren't united by anything they share other than 
+#' that they fill a need in my personal apps.
+#'
+#'
+#' @docType package
+#' @name shinyAddOns
+NULL
