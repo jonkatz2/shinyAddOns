287a8e63476c5d6583d3c0e20f580d272dd1c0a4
jonkatz2 <jonkatz2@gmail.com>
2016-02-21 16:49:41 -0500

    update

--- R/popoverInput.R
+++ R/popoverInput.R
@@ -1,6 +1,6 @@
 #' popoverInput
 #' 
-#' Place a help icon with popover text next to a shiny input function
+#' Place a help icon with popover text next to a shiny input function using an actionLink 
 #' 
 #' 
 #' @param fnType function or function name (character) of desired input, e.g. textInput or 'textInput'
@@ -59,5 +59,69 @@ popoverInput <- function(
         shiny::column(width=colWidths[2], 
             do.call(fnType,c(list(inputId=id,label=label), fnParams))
         ) 
+    )
+}
+
+
+#' popoverInput2
+#' 
+#' Place a help icon with popover text next to a shiny input function. Both the help icon and the input element float left.
+#' 
+#' 
+#' @param fnType function or function name (character) of desired input, e.g. textInput or 'textInput'
+#' @param id InputID for shiny element (character).
+#' @param label Input label (character).
+#' @param \dots Additional named arguments to input function.
+#' @param helpText Text to place in popover (character).
+#' @param fa.icon Icon name from font awesome, default is "fa-question-circle".
+#' @param fa.color Color of icon; defaults to bootstrap link color.
+#' @param padding.top Padding to align the help icon vertically with input label.
+#' @param popTrigger Trigger for popover. In c('hover','focus','click','manual').
+#' @param container HTML container to hold the input, either \code{div} or \code{span}
+#' @param cStyle Inline CSS for main container returned (character or NULL).
+#' @return HTML code. 
+#' @author Jon Katz
+#' @note Requires Eric Bailey's shinyBS package (available from CRAN).
+#' @examples
+#'    popoverInput(
+#'        checkboxGroupInput, 
+#'        'lyrOpts', 
+#'        'Options:',
+#'        choices=c(
+#'            'Paste URL'='url',
+#'            'Clip to Layer'='clip', 
+#'            'Projection Ref'='projection', 
+#'            'No Data'='empty'
+#'            ), 
+#'        selected=c('clip','projection'), 
+#'        inline=TRUE,
+#'        helpText="To download a layer from a website check \"Paste URL\". You may also clip or project all rasters to match this one. If the layer contains only boundary or projection information check \"No Data\".",
+#'        divStyle='padding-top:1em;padding-bottom:1em;',
+#'        helpStyle='padding-top:0.5em;'
+#'    )
+#' @keywords manip
+#' @export
+#' 
+#' 
+
+
+popoverInput2 <- function(
+    fnType,
+    id,
+    label,
+    ...,
+    helpText,
+    fa.icon='fa-question-circle',
+    fa.color='#337ab7',
+    padding.top='0.3em',
+    popTrigger='hover',
+    container=shiny::div,
+    cStyle='clear:both;'
+) {
+    fnParams <- list(...)
+    container(style=cStyle,
+        shiny::HTML(paste0('<i id=', paste0(id,'Help'), ' class="fa ', fa,'" style="float:left; padding:',padding.top, ' 0.3em; color:', fa.color,'";></i>')),
+        shinyBS::bsPopover(id=paste0(id,'Help'), title=NULL, content=helpText, trigger=popTrigger, options=list(container='body')),
+        shiny::span(style='float:left;', do.call(fnType,c(list(inputId=id,label=label), fnParams)))
     )
 }
