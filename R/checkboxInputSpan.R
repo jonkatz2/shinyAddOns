2fbe03d8ad8bae7fc53fa360d862f98280c6c234
jonkatz2 <jonkatz2@gmail.com>
2016-04-25 21:14:36 -0400

    update

---
+++ R/checkboxInputSpan.R
@@ -0,0 +1,10 @@
+checkboxInputSpan <- function (inputId, label, value = FALSE, width = NULL) 
+{
+    inputTag <- shiny::tags$input(id = inputId, type = "checkbox")
+    if (!is.null(value) && value) 
+        inputTag$attribs$checked <- "checked"
+    shiny::span(class = "form-group shiny-input-container", style = if (!is.null(width)) 
+        paste0("width: ", shiny::validateCssUnit(width), ";"), shiny::div(class = "checkbox", 
+        shiny::tags$label(inputTag, shiny::tags$span(label))))
+}
+
